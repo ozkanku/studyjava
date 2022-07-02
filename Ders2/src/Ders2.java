cca47c8dc9cb9423418890e290a4c0513dd62208
ozkanku <kultiginn3@gmail.com>
2022-07-02 15:34:27 +0300

    ders2

---
+++ Ders2/src/Ders2.java
@@ -0,0 +1,26 @@
+
+public class Ders2 {
+
+	public static void main(String[] args) {
+		int a=51;
+		short s=32;
+		double d=3.14;
+		char c='a';
+		boolean b=true;
+		String murat="murat";
+		
+		
+		
+		
+		
+		
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(murat);
+
+	}
+
+}
