810b1b78d679f581892249390bb681b96145cd51
ozkanku <kultiginn3@gmail.com>
2022-07-05 15:57:54 +0300

    ders

---
+++ Ders3/src/Ders3.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ders3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		scan.useLocale(Locale.US);
+		//System.out.println("bir sayi giriniz");
+		/*
+		int a=scan.nextInt();
+		
+		System.out.println("a değişkeninin değeri:"+a);
+		*/
+		
+		// double a=scan.nextDouble();
+		System.out.println("bir yazı giriniz");
+		String a=scan.nextLine();
+		
+		
+		System.out.println("a değişkeninin değeri:"+a);
+		
+		
+		
+		
+	}
+
+}
