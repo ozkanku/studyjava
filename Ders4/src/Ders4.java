1ba0e59800e0d985cbf9bed766b7e17e1b201b00
ozkanku <kultiginn3@gmail.com>
2022-07-05 18:17:56 +0300

    ders4

--- Ders4/src/Ders4.java
+++ Ders4/src/Ders4.java
@@ -3,7 +3,25 @@ public class Ders4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int x=10;
+		int y=4;
+		System.out.println("sonuc: "+(x+y));
+		
+		System.out.println("sonuc: "+(x*y));
+		
+		System.out.println("sonuc: "+(x/y));
+		
+		System.out.println("sonuc: "+((double)x/y));
+		
+		System.out.println("sonuc: "+(x%y));
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
