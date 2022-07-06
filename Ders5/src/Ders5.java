81ced1807c45d1784e4436652eee3f21786532dc
ozkanku <kultiginn3@gmail.com>
2022-07-06 11:57:53 +0300

    ders5

---
+++ Ders5/src/Ders5.java
@@ -0,0 +1,28 @@
+
+public class Ders5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		a=a+1;// ==  a+=1
+		
+		System.out.println("a nın değeri "+a);
+		
+		int b=10;
+		System.out.println("b nin değeri "+b++);
+		
+		System.out.println("b nin değeri "+b);
+		
+		int c=10;
+		--c;
+		System.out.println("c nin değeri "+c);
+		
+		
+		
+		
+		
+	}
+
+}
