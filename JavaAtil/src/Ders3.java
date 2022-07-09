71f96ee9df303b31bd7e8dc2a01582259550f37b
ozkanku <kultiginn3@gmail.com>
2022-07-09 09:11:56 +0300

    ders3

---
+++ JavaAtil/src/Ders3.java
@@ -0,0 +1,18 @@
+
+public class Ders3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Final const benzeri 
+		final int myInteger=5;
+		System.out.println("myInteger: "+myInteger);
+		
+		//myInteger=4;
+		
+		System.out.println("myInteger: "+myInteger);
+		
+
+	}
+
+}
