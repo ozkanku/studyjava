e6c9b5e6e21c5ecb3cb0e683387b3c9e7625d136
ozkanku <kultiginn3@gmail.com>
2022-07-09 09:10:27 +0300

    Ders8

---
+++ Ders8/src/Ders8.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Ders8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("sayi gir");
+		/*int sayi=scan.nextInt();
+		
+		
+		
+		switch(sayi) {
+			case 1:
+				System.out.println("bir");
+				break;
+			case 2:
+				System.out.println("iki");
+				break;
+			case 3:
+				System.out.println("üç");
+				break;
+			default:
+				System.out.println("bir iki üç değil");
+				break;
+			
+		}
+		
+		*/
+		int sayi=scan.nextInt();
+		int bakiye=1000;
+		int islem;
+		islem=scan.nextInt();
+		
+		switch(islem) {
+			case 1:
+				System.out.println("bakiyeniz"+bakiye+" tl dir");
+				break;
+			case 2:
+				System.out.println();
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
