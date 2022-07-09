0b99d6ed30a69c7cc57a36e7d41ae7f73f7f71e7
ozkanku <kultiginn3@gmail.com>
2022-07-09 09:11:45 +0300

    ders1

---
+++ JavaAtil/src/Ders1.java
@@ -0,0 +1,18 @@
+
+public class Ders1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=20;
+		System.out.println(10*age);
+		System.out.println(age/5);
+		
+		int x=5;
+		int y=11;
+		System.out.println((float)y/x);
+		
+		float myfloat=10.0f;
+
+	}
+
+}
