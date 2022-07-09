067c7c698b64471a7c7db7ccbe576e07df842f8a
ozkanku <kultiginn3@gmail.com>
2022-07-09 09:11:50 +0300

    ders2

---
+++ JavaAtil/src/Ders2.java
@@ -0,0 +1,34 @@
+import java.net.MulticastSocket;
+
+public class Ders2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double pi=3.14;
+		int r=5;
+		System.out.println(2*r*pi);
+		
+		
+		
+		//String
+		
+		String name="James";
+		String surname="Hetfield";
+		System.out.println(name);
+		System.out.println(surname);
+		
+		String fullname=name+" "+surname;
+		System.out.println(fullname);
+		
+		//Boolean
+		
+		boolean isAlive=true;
+		isAlive=false;
+		System.out.println(isAlive);
+		
+		
+		
+	}
+
+}
